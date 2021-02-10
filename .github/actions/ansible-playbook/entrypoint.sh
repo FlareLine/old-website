573dd94c73b6487e8553b098f47c39411ad1a75d
Ethan Wright <ethan@flareline.com>
2021-02-11 06:34:09 +1100

    Modify entrypoint

--- .github/actions/ansible-playbook/entrypoint.sh
+++ .github/actions/ansible-playbook/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-if [ -z "$requirements" ]
+playbook=$1
+
+if [ -z "$2" ]
 then
   echo "No requirements specified."
 else
+  requirements=$2
   echo "Installing galaxy requirements '${requirements}'."
   ansible-galaxy install -r ${requirements}
 fi
